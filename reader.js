f9758129561dd14c9f35d26182d7d42463534e39
andrewpp26 <54845709+andrewpp26@users.noreply.github.com>
2020-05-20 13:54:13 +0200

    Update reader.js

--- reader.js
+++ reader.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("button").click(function(){
-    $.get("http://192.168.1.109:8080/recargaServer/api/products", function(data, status){
+    $.get("https://192.168.1.109:8080/recargaServer/api/products", function(data, status){
       alert("Data: " + data + "\nStatus: " + status);
     });
   });
